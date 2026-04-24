fc3d5aa97f79e0c9addf3efa51e5f533fe63de82
enzo <enzolibman@gmail.com>
2026-04-24 09:48:20 -0300

    feat:html semantico

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,19 @@ const texto=titulolivre
 
 
 console.log(texto)
+
+// to fixed
+
+const preçoproduto = 199.99;
+const desconto = 0.15; //15%
+const preçofinal =preçoproduto*(1/desconto);
+console.log(preçofinal)
+console.log(`r$ ${preçofinal.toFixed(2)}`);
+//metodo de array
+const produtos=[
+    {nome:"teclado mecanico", preço:200,promoçao:true},
+    {nome:"mouse gamer", preço:500,promoçao:false},
+    {nome:"fone gay", preço:1000,promoçao:true},
+    {nome:"mousepad", preço:100,promoçao:false},
+]
+console.log(produtos)
